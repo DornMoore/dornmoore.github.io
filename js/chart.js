2e3a1e3eef34bf28fa44b09f8bd017c595a6f42e
@DornMoore <dorn@savingcranes.org>
2018-11-08 16:49:31 -0600

    updayes!

---
+++ js/chart.js
@@ -0,0 +1,153 @@
+// setup 
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    w = 960 - margin.left - margin.right,
+    h = 600 - margin.top - margin.bottom;
+
+var parseYear = d3.timeParse("%Y");
+
+// setup x 
+var xValue = function(d) { return parseInt(d.count_yr);}, // data -> value
+    xScale = d3.scaleLinear().range([0, w]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.axisBottom(xScale);
+
+// setup y
+var yValue = function(d) { return parseFloat(d.lat);}, // data -> value
+    yScale = d3.scaleLinear().range([h, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.axisLeft(yScale);
+
+// add the graph canvas to the body of the webpage
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// load data
+d3.queue()
+  .defer(d3.csv, "data/cbc_gmc_by_year.csv")
+  .defer(d3.csv, "data/qry_cbc_count_by_year.csv")
+
+  .await(callback);
+
+function callback(error, gmc_data, sacr_cir) {
+    if(error) throw error;
+    // console.log(gmc_data);
+    
+
+    // don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain([d3.min(gmc_data, xValue)-1, d3.max(gmc_data, xValue)+1]);
+    yScale.domain([d3.min(sacr_cir, yValue)-1, d3.max(sacr_cir, yValue)+1]);
+    // console.log(d3.min(gmc_data[count_yr]));
+
+    // x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", w)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Year");
+
+    // y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Latitude");
+
+    var line = d3.line()
+        .x(function(d) {return xScale(d.count_yr);})
+        .y(function(d) {return yScale(d.lat);})
+        .curve(d3.curveMonotoneX);
+
+    
+    // draw dots for 
+    svg.selectAll(".cirdot")
+        .data(sacr_cir)
+        .enter()
+        .append("circle")
+        .attr("class", "cirdot")
+        .attr("r", function(d){ //set the dot radius
+          //use the length of the value as a proxy for classes
+          return (d.num_sacr).length * 1.2;
+        })
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        // .style("fill", function(d) { return color(cValue(d));}) 
+        .style("fill", "#D3D3D3")
+        .style("stroke", "white")
+        .style("stroke-width", "0.75")
+        .on("mouseover", function(d) {
+            tooltip.transition()
+                 .duration(200)
+                 .style("opacity", .7);
+            tooltip.html(d.circle_name + "</br>"+d.num_sacr+" Cranes")
+                 .style("left", (d3.event.pageX + 5) + "px")
+                 .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                 .duration(400)
+                 .style("opacity", 0);
+        });
+
+    // Draw the line showing the change in GMC each year.
+    svg.append("path")
+      .datum(gmc_data)
+      .attr("class", "line")
+      .attr("d", line)
+      .style("fill", "none")
+      .style("stroke", "#ffab00")
+      .style("stroke-width","3");
+
+
+    // draw dots for 
+    svg.selectAll(".gmcdot")
+        .data(gmc_data)
+        .enter()
+        .append("circle")
+        .attr("class", "gmcdot")
+        .attr("r", 2)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        // .style("fill", function(d) { return color(cValue(d));}) 
+        .style("fill", "#ffab00");        ;
+
+    // // draw legend
+    // var legend = svg.selectAll(".legend")
+    //     .gmc_data(color.domain())
+    //   .enter().append("g")
+    //     .attr("class", "legend")
+    //     .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    // // draw legend colored rectangles
+    // legend.append("rect")
+    //     .attr("x", w - 18)
+    //     .attr("width", 18)
+    //     .attr("height", 18)
+    //     .style("fill", color);
+
+    // // draw legend text
+    // legend.append("text")
+    //     .attr("x", w - 24)
+    //     .attr("y", 9)
+    //     .attr("dy", ".35em")
+    //     .style("text-anchor", "end")
+    //     .text(function(d) { return d;})
+};
